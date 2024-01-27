b471d7f991979a49c1b6327e145a0be2d64947ad
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2024-01-27 16:18:27 +0000

    Added project components commit

---
+++ src/pages/ProjectGallery.jsx
@@ -0,0 +1,15 @@
+import Project from "../components/Project";
+import projectsData from "../data/projects-data.json"
+
+function ProjectGallery() {
+    // Body
+    return <div>
+        <div>
+            {projectsData.map((project) => {
+                return <Project key={project.id} title={project.title} description={project.description} image={project.image}/>
+            })}
+        </div>
+    </div>
+}
+
+export default ProjectGallery;
