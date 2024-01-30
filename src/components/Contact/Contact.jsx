d9fb56cc980cf4eac8114869f5870de0b4151e25
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2024-01-30 16:56:38 +0000

    Fixed mobile success contact message

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -110,9 +110,8 @@ const Contact = () => {
                                 <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
                             </svg>
                         </button>
-                    </form>
 
-                    {/* Showing success message if the email is sent successfully */}
+                        {/* Showing success message if the email is sent successfully */}
                     {showSuccessMessage && (
                         <p className="success-message">
                             Your message was sent. I will get back to you as soon as possible.
@@ -121,6 +120,7 @@ const Contact = () => {
                             <i className="bi bi-check-circle"></i>
                         </p>
                     )}
+                    </form>
                 </div>
             </div>
         </section>
