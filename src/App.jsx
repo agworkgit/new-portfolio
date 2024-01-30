f3d3397fbed959d00b60e7e8eb409c33ed8e360c
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2024-01-30 03:32:53 +0000

    Swapped to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { createContext, useState, useEffect } from 'react';
 import './App.css';
 
