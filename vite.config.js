7571dd958143d3dffdab4b6b35237d0402441c5a
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2024-02-01 23:37:32 +0000

    Added favicon

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/new-portfolio/",
-  plugins: [react()],
+  plugins: [react(
+    vitePluginFaviconsInject('./public/icons/favicon.png')
+  )],
 })
